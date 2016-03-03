b0f907f40955305a31ea9b118009cc6a8ca7d54d
Povilas Arlauskas <povilas.arl@gmail.com>
2016-03-03 08:40:28 +0200

    Init
    Pirmas commit

---
+++ src/main/java/lt/vu/psk/entities/Course.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.vu.psk.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Povilas
+ */
+@Entity
+@Table(name = "COURSE")
+@NamedQueries({
+    @NamedQuery(name = "Course.findAll", query = "SELECT c FROM Course c"),
+    @NamedQuery(name = "Course.findById", query = "SELECT c FROM Course c WHERE c.id = :id"),
+    @NamedQuery(name = "Course.findByName", query = "SELECT c FROM Course c WHERE c.name = :name"),
+    @NamedQuery(name = "Course.findByOptLockVersion", query = "SELECT c FROM Course c WHERE c.optLockVersion = :optLockVersion"),
+    @NamedQuery(name = "Course.findByCourseId", query = "SELECT c FROM Course c WHERE c.courseId = :courseId")})
+public class Course implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Size(max = 50)
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "OPT_LOCK_VERSION")
+    private Integer optionalLockVersion;
+    @Size(max = 20)
+    @Column(name = "COURSE_ID")
+    private String courseId;
+    @ManyToMany(mappedBy = "courseList")
+    private List<Student> studentList;
+
+    public Course() {
+    }
+
+    public Course(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getOptionalLockVersion() {
+        return optionalLockVersion;
+    }
+
+    public void setOptionalLockVersion(Integer optionalLockVersion) {
+        this.optionalLockVersion = optionalLockVersion;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (courseId != null ? courseId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Student)) {
+            return false;
+        }
+        Course other = (Course) object;
+        return this.courseId.equals(other.courseId);
+    }
+
+    @Override
+    public String toString() {
+        return "lt.vu.psk.entities.Course[ id=" + id + " ]";
+    }
+    
+}
